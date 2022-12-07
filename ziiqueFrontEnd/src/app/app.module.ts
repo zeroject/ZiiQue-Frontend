098b3c658484bae802e81d2357904002bb2ef351
Ominousity <abs@hotmail.dk>
2022-12-07 12:37:06 +0100

    declared profile page component

--- ziiqueFrontEnd/src/app/app.module.ts
+++ ziiqueFrontEnd/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { NewUserPageComponent } from './new-user-page/new-user-page.component';
 import { BeatMakerPageComponent } from './beat-maker-page/beat-maker-page.component';
+import { ProfilePageComponent } from './beat-maker-page/profile-page/profile-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
@@ -22,25 +21,17 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 
 
 const routes: Routes = [
-  { path: 'Profile', component: ProfilePageComponent, canActivate: [AuthguardService]},
   { path: 'Login', component: LoginPageComponent },
-  { path: 'NewUser', component: NewUserPageComponent },
   { path: 'BeatMaker', component: BeatMakerPageComponent },
   { path: '**', redirectTo:'Login'},
-  { path: 'Profile', pathMatch: "prefix", component: ProfilePageComponent, canActivate: [AuthguardService] },
-  { path: 'Login', pathMatch: "full", component: LoginPageComponent },
-  { path: 'NewUser', pathMatch: "prefix", component: NewUserPageComponent },
-  { path: 'BeatMaker', pathMatch: "prefix", component: BeatMakerPageComponent},
-  { path: '**', redirectTo:'/BeatMaker', pathMatch: "full"}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfilePageComponent,
     LoginPageComponent,
-    NewUserPageComponent,
-    BeatMakerPageComponent
+    BeatMakerPageComponent,
+    ProfilePageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
